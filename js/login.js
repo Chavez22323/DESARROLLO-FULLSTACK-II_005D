c6c0e7d4aecac1eb3c4862404389e0f439aae22f
MdeMedina <mamedina770@gmail.com>
2025-09-09 09:50:35 -0300

    cambios pequeños en el main

---
+++ js/login.js
@@ -0,0 +1,24 @@
+
+    const form = document.getElementById('loginForm');
+    const passwordInput = document.getElementById('password');
+    const emailInput = document.getElementById('email');
+    const errorMsg = document.getElementById('error');
+
+    form.addEventListener('submit', function(event) {
+      errorMsg.textContent = ''; 
+
+      if (passwordInput.value.length < 8) {
+        errorMsg.textContent = 'La contraseña debe tener mínimo 8 caracteres';
+        event.preventDefault();
+        return;
+      }
+
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailPattern.test(emailInput.value)) {
+        errorMsg.textContent = 'Debes ingresar un correo electrónico válido';
+        event.preventDefault();
+        return;
+      }
+      window.location.href = '/index.html';
+      alert('Inicio de sesión exitoso');
+    });
